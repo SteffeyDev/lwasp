a972d7b8f7272171a4ecc15e00cc2fb95fdf1227
SteffeyDev <steffeydev@icloud.com>
2015-08-04 19:54:05 -0500

    deleting duplicates

--- emailz.py
+++
@@ -1,5 +0,0 @@
-from utility import sendScoringReport
-from utility import notify
-
-sendScoringReport()
-notify('Sent!', 'Your Scoring Report has been sent to the administrator')
--- open.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sudo python ip.py
-firefox /usr/ScoringEngine/ScoringReport.html
