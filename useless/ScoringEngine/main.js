b354765c545245eba165c76eed246c7780c8c913
SteffeyDev <steffeydev@icloud.com>
2015-08-04 22:24:29 -0500

    Get IP in Javascript

--- useless/ScoringEngine/main.js
+++ useless/ScoringEngine/main.js
@@ -20,6 +20,8 @@ function writeToFile(settings) {
     settingsFile.close();
 }
 
+
+
 //basically contains everywhere
 var Main = React.createClass({
 	componentDidMount: function() {
@@ -27,7 +29,58 @@ var Main = React.createClass({
     this.readSettingsFile();
     window.addEventListener("resize", this.updateDims);
     this.updateFile(); // starts recursize refresh
+    this.getIP();
 	},
+  getIP: function() {
+    var parent = this;
+    //NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+    var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+    if (RTCPeerConnection) (function () {
+        var rtc = new RTCPeerConnection({iceServers:[]});
+        if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+            rtc.createDataChannel('', {reliable:false});
+        };
+
+        rtc.onicecandidate = function (evt) {
+            // convert the candidate to SDP so we can run it through our general parser
+            // see https://twitter.com/lancestout/status/525796175425720320 for details
+            if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+        };
+        rtc.createOffer(function (offerDesc) {
+            grepSDP(offerDesc.sdp);
+            console.log(offerDesc.sdp);
+            rtc.setLocalDescription(offerDesc);
+        }, function (e) { parent.setState({"ip": ""}); });
+
+        var addrs = Object.create(null);
+        addrs["0.0.0.0"] = false;
+        function updateDisplay(newAddr) {
+            if (newAddr in addrs) return;
+            else addrs[newAddr] = true;
+            var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
+            parent.setState({"ip": (displayAddrs.join(", ") || "n/a")});
+        }
+
+        function grepSDP(sdp) {
+            var hosts = [];
+            sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+                if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+                    var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                        addr = parts[4],
+                        type = parts[7];
+                    if (type === 'host') updateDisplay(addr);
+                } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+                    var parts = line.split(' '),
+                        addr = parts[2];
+                    updateDisplay(addr);
+                }
+            });
+        }
+    })(); else {
+      parent.setState({"ip": ""});
+    }
+  },
   updateFile: function() {
     var parent = this;
     setInterval(function() {
@@ -56,7 +109,8 @@ var Main = React.createClass({
 	getInitialState: function () {
 		return {
 			"vulnerabilities": [],
-      "settings": {}
+      "settings": {},
+      "ip":""
 		}
 	},
   setTeamId: function(button) {
@@ -199,6 +253,12 @@ var Main = React.createClass({
       }
     }
 
+    var ipStr = this.state.ip;
+    var ip = null;
+    if (ipStr != '') {
+      ip = <h3 style={centerStyle}>Image IP Address: {ipStr}</h3>;
+    }
+
 		return (
 			<div style={backDivStyle}>
 				<div style={containerDivStyle}>
@@ -208,7 +268,7 @@ var Main = React.createClass({
           {limit == -1 ? null : <h3 style={limitStyle2}>Image Running Time: {runningTimeLabel}</h3>}
           {limit == -1 ? null : <h3 style={limitStyle}>Image Time Remaining: {timeLeftLabel}</h3>}
           {id}
-          {{settings['ip'] == '' ? null : <h3 style={centerStyle}>Image IP Address: {settings['ip']}</h3>}
+          {ip}
           <h2 style={centerStyle}>{numberOfPointsRecieved} out of {numberOfPointsTotal} points received</h2>
 					<br></br>
 					<h3 style={leftStyle}>Connection Status: <span style={greenStyle}>Scoring Data Uploaded Successfully: No Errors Detected</span></h3>
