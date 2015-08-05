b71d793d7543d1d48820d07ffe915babfd9d7ecf
SteffeyDev <steffeydev@icloud.com>
2015-08-04 19:49:53 -0500

    deleting duplicates

--- cron.bash
+++
@@ -1,7 +0,0 @@
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file # refresh calls analyze.py (setup in initialize.py)
-echo "*/1 * * * * sudo refresh" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
--- cse.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/python /Users/petersteffey/Box\ Sync/Scoring\ Engine/restart.py
