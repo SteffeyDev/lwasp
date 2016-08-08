c42f2c2a8d7fdbc41a30fec0cf2df9911ae88e02
Peter Steffey <steffeydev@icloud.com>
2016-08-07 20:09:30 -0700

    restructure

--- useless/emailz.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2015 Peter Steffey
-
-from utility import sendScoringReport
-from utility import notify
-
-sendScoringReport()
-notify('Sent!', 'Your Scoring Report has been sent to the administrator')
--- useless/open.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright (C) 2015 Peter Steffey
-
-#sudo python ip.py
-firefox /usr/ScoringEngine/ScoringReport.html
